1440069ab2bebfb1339c8d34d90ff049b65989d1
Ábel Szilágyi <szila.abel@gmail.com>
2021-12-30 22:10:05 +0100

    changed the  world.

--- DjBahama/Program.cs
+++ DjBahama/Program.cs
@@ -6,7 +6,7 @@ namespace DjBahama
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Nigger!");
         }
     }
 }
